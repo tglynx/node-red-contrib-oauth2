b7c25f3c71a729dcce8c0510bdacf27f5739c6c3
Michael Sommer <github@neuronetix.de>
2023-07-13 20:36:31 +0200

    Implemented new grant type set_by_config_node Initial labels for oauth2Credentials Node Editor logic for new grant type Implemented correct fields for initial config node in template implemented initial logic in oauth2Credentials.js

--- src/oauth2Credentials.js
+++ src/oauth2Credentials.js
@@ -1,8 +1,17 @@
 module.exports = function(RED) {
-    function RemoteServerNode(n) {
+    function oauth2CredentialsNode(n) {
         RED.nodes.createNode(this,n);
-        this.host = n.host;
-        this.port = n.port;
+
+        this.name = n.name || '';
+      
+        this.access_token_url = n.access_token_url || '';
+        this.grant_type = n.grant_type || '';
+        this.username = n.username || '';
+        this.password = n.password || '';
+        this.client_id = n.client_id || '';
+        this.client_secret = n.client_secret || '';
+        this.scope = n.scope || '';
+        this.resource = n.resource || '';
     }
-    RED.nodes.registerType("oauth2Credentials",RemoteServerNode);
+    RED.nodes.registerType("oauth2Credentials",oauth2CredentialsNode);
 }
