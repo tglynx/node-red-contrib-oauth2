0d3fc36cf640fcb43f2463b28f2f90e44e72eb4a
Michael Sommer <github@neuronetix.de>
2023-07-14 14:29:55 +0200

    oauth2 credentials config node prototype

--- src/oauth2Credentials.js
+++ src/oauth2Credentials.js
@@ -2,16 +2,25 @@ module.exports = function(RED) {
     function oauth2CredentialsNode(n) {
         RED.nodes.createNode(this,n);
 
-        this.name = n.name || '';
+        this.name = n.name;
       
-        this.access_token_url = n.access_token_url || '';
         this.grant_type = n.grant_type || '';
+        this.access_token_url = n.access_token_url || '';
+        this.authorization_endpoint = n.authorization_endpoint || '';
+        this.redirect_uri = n.redirect_uri || '';
+        this.open_authentication = n.open_authentication || '';
         this.username = n.username || '';
         this.password = n.password || '';
         this.client_id = n.client_id || '';
         this.client_secret = n.client_secret || '';
         this.scope = n.scope || '';
         this.resource = n.resource || '';
+
     }
-    RED.nodes.registerType("oauth2Credentials",oauth2CredentialsNode);
+    RED.nodes.registerType("oauth2Credentials",oauth2CredentialsNode, {
+        credentials: {
+            password: {type:"password"},
+            client_secret: {type:"password"}
+        }
+    });
 }
